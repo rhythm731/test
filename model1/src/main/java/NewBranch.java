c5e3df6f3a0e815525f04e54adfd84e41ab48104
wuliang05 <601268901@qq.com>
2021-01-17 15:28:37 +0800

    new Branch

---
+++ model1/src/main/java/NewBranch.java
@@ -0,0 +1,12 @@
+/**
+ * Description :
+ *
+ * @author : wuliang05
+ * @date : 2021/1/17 15:27
+ */
+public class NewBranch {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
