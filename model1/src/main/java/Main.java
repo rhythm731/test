b432017d02833c72a4467499fe21b82f592e98a8
wuliang05 <601268901@qq.com>
2021-01-17 15:13:36 +0800

    first

---
+++ model1/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Description :
+ *
+ * @author : wuliang05
+ * @date : 2021/1/17 15:11
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+
+    }
+}
